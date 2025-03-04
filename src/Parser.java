122df5421bfe3e1b83eb8ccc8144e6649288fcd3
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-03 22:46:23 -0600

    Se le agrega unos comentarios a la clase

--- src/Parser.java
+++ src/Parser.java
@@ -1,10 +1,16 @@
+/*
+ * Parser utiliza los tockens generados en exter y los ordena y estructura para que luego pasen a evaluator
+ * (3/03/2025)
+ * @authors
+ */
+
+import java.util.List;
+
 public class Parser {
-    private List<String> tokens; // Definimos tokens 
+    private List<String> tokens; // Definimos tokens
     private int position;
 
-
-// Organiza la estructura
-// Toma las ecuaciones y las ejecuta  
-  
+    // Organiza la estructura
+    // Toma las ecuaciones y las ejecuta
 
 }
