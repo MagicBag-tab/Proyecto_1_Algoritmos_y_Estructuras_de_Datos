828b785f3c0b14bea46d9e16a2504d7db69cee6c
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-03 22:18:03 -0600

    Se agrega el metodo de tokenizer, el cual crea cada tocken y dependiendo de si encuentra un parentesis, espacio o un operador, simbolo, valor, lo pone dentro del mismo token o crea un nuevo token

--- src/Lexer.java
+++ src/Lexer.java
@@ -20,4 +20,27 @@ public class Lexer {
 
     }
 
+    /*
+     * Tokenizer separa la cadena que se genera a partir del file (codigo de lisp) y
+     * revisa que esten los parentesis y si hay espacios que separan a los
+     * operadores.
+     */
+    public void Tokenizer() {
+        StringBuilder token = new StringBuilder(); // cadena de caracteres donde se almacenan los tockens
+
+        for (int i = 0; i < file.length(); i++) {
+            char c = file.charAt(i); // cada caracter del file
+
+            if (c == '(' || c == ')') {
+                if (token.length() > 0) {
+                    tokens.add(token.toString()); // agreca el token a el arraylist de tockens
+                    token.setLength(0); // reinicia el tocken ya que indican el inicio o el final de una instrucción
+                }
+            } else if (file.isEmpty()) {
+
+            }
+        }
+
+    }
+
 }
