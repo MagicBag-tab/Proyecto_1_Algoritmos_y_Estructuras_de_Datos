ddcee0d7fac14779a1fcaa4b995ca3fc6f66d266
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-06 18:36:01 -0600

    Se le agrega su constructor

--- src/Parser.java
+++ src/Parser.java
@@ -1,6 +1,6 @@
 /*
- * Parser utiliza los tockens generados en exter y los ordena y estructura para que luego pasen a evaluator
- * (3/03/2025)
+ * Parser utiliza los tockens generados en lexter y los ordena y estructura para que luego pasen a evaluator
+ * (3/03/2025) (6/03/2025)
  * @authors
  */
 
@@ -8,9 +8,14 @@ import java.util.List;
 
 public class Parser {
     private List<String> tokens; // Definimos tokens
-    private int position;
+    private int position; // utilizada para saber la posición en donde se encuentra en la lista
 
-    // Organiza la estructura
-    // Toma las ecuaciones y las ejecuta
+    /*
+     * Constructor de Parse (6/03/2024)
+     */
+    public Parser(List<String> tokens) {
+        this.tokens = tokens;
+        this.position = 0;
+    }
 
 }
