91e4c968dd60b9b1a25345fe65edf91cfa66f4c6
MagicBag-tab <est24347@uvg.edu.gt>
2025-03-06 19:54:57 -0600

    Se le agregan los metodos de Atom y parse, atom ve si es int o simbolo y ya parse crea toda la estructura a partir de los tokens que recibe

--- src/Parser.java
+++ src/Parser.java
@@ -4,6 +4,7 @@
  * @authors
  */
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Parser {
@@ -16,6 +17,53 @@ public class Parser {
     public Parser(List<String> tokens) {
         this.tokens = tokens;
         this.position = 0;
+    }
+
+    /*
+     * Verifica si el token es un entero o si es un
+     * símbolo y lo devuelve para ser utilizado en el metodo de parse.
+     */
+    public Object Atom(String token) {
+        if (token.matches("-?\\d+")) {
+            return Integer.parseInt(token);
+        }
+        return token;
+    }
+
+    /*
+     * Convierte los tokens en una estructura de listas utilizando los
+     * paréntesis para definir la estructura correcta y verifica que
+     * este colocados correctamente sin que haya un error de sintaxis
+     * en el código enviado.
+     */
+
+    @SuppressWarnings("unchecked")
+    public Object parse() {
+        List<Object> stack = new ArrayList<>();
+        List<Object> current = new ArrayList<>();
+
+        for (position = 0; position < tokens.size(); position++) {
+            String token = tokens.get(position);
+
+            if (token.equals("(")) {
+                stack.add(current);
+                current = new ArrayList<>();
+            } else if (token.equals(")")) {
+                if (stack.isEmpty()) {
+                    throw new RuntimeException("Error de sintaxís");
+                }
+                List<Object> complete = current;
+                current = (List<Object>) stack.remove(stack.size() - 1);
+                current.add(complete);
+            } else {
+                current.add(Atom(token));
+            }
+        }
+        if (!stack.isEmpty()) {
+            throw new RuntimeException("Error de sintaxís");
+        }
+
+        return current;
     }
 
 }
