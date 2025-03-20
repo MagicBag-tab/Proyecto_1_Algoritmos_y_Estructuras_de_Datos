7d85cb9d3fe9c9b52a657294c8c38666869f5fbd
Luis <langelgirona@gmail.com>
2025-03-20 16:46:58 -0600

    Cambiar EvaluatorTest para verificar errores

--- src/EvaluatorTest.java
+++ src/EvaluatorTest.java
@@ -57,34 +57,63 @@ public class EvaluatorTest {
     @Test
     public void testEvaluateEqual() {
         Evaluator evaluator = new Evaluator();
-        assertTrue((Boolean) evaluator.evaluate(List.of("EQUAL", 2, 2)));
-        assertFalse((Boolean) evaluator.evaluate(List.of("EQUAL", 2, 3)));
+        assertEquals("T", evaluator.evaluate(List.of("EQUAL", 2, 2)));
+        assertEquals("NIL", evaluator.evaluate(List.of("EQUAL", 2, 3)));
     }
 
     @Test
     public void testEvaluateAtom() {
         Evaluator evaluator = new Evaluator();
-        assertTrue((Boolean) evaluator.evaluate(List.of("ATOM", 2)));
-        assertFalse((Boolean) evaluator.evaluate(List.of("ATOM", List.of(1, 2))));
+        assertEquals("T", evaluator.evaluate(List.of("ATOM", 2)));
+        assertEquals("NIL", evaluator.evaluate(List.of("ATOM", List.of(1, 2))));
     }
 
     @Test
     public void testEvaluateList() {
         Evaluator evaluator = new Evaluator();
-        assertTrue((Boolean) evaluator.evaluate(List.of("LIST", 1, 2, 3)));
-    }//Cambiar test
+        assertEquals("T", evaluator.evaluate(List.of("LIST", List.of(1, 2, 3))));
+    }
 
     @Test
     public void testEvaluateLessThan() {
         Evaluator evaluator = new Evaluator();
-        assertTrue((Boolean) evaluator.evaluate(List.of("<", 2, 3)));
-        assertFalse((Boolean) evaluator.evaluate(List.of("<", 3, 2)));
+        assertEquals("T", evaluator.evaluate(List.of("<", 2, 3)));
+        assertEquals("NIL", evaluator.evaluate(List.of("<", 3, 2)));
     }
 
     @Test
     public void testEvaluateGreaterThan() {
         Evaluator evaluator = new Evaluator();
-        assertTrue((Boolean) evaluator.evaluate(List.of(">", 3, 2)));
-        assertFalse((Boolean) evaluator.evaluate(List.of(">", 2, 3)));
+        assertEquals("T", evaluator.evaluate(List.of(">", 3, 2)));
+        assertEquals("NIL", evaluator.evaluate(List.of(">", 2, 3)));
+    }
+
+    @Test
+    public void testEvaluateDefun() {
+        Evaluator evaluator = new Evaluator();
+        evaluator.evaluate(List.of("DEFUN", "square", List.of("x"), List.of("*", "x", "x")));
+        assertEquals(9, evaluator.evaluate(List.of("square", 3)));
+    }
+
+    @Test
+    public void testEvaluateUserDefinedFunction() {
+        Evaluator evaluator = new Evaluator();
+        evaluator.evaluate(List.of("DEFUN", "add", List.of("x", "y"), List.of("+", "x", "y")));
+        assertEquals(5, evaluator.evaluate(List.of("add", 2, 3)));
+    }
+
+    @Test
+    public void testEvaluateNestedExpressions() {
+        Evaluator evaluator = new Evaluator();
+        assertEquals(14, evaluator.evaluate(List.of("+", 2, List.of("*", 3, 4))));
+    }
+
+    @Test
+    public void testEvaluateUndefinedVariable() {
+        Evaluator evaluator = new Evaluator();
+        Exception exception = assertThrows(IllegalArgumentException.class, () -> {
+            evaluator.evaluate("undefinedVar");
+        });
+        assertEquals("¡Advertencia! Tu Variable no ha sido definida: undefinedVar", exception.getMessage());
     }
 }
